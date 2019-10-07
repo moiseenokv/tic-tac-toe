c88390c3f76e379896c13fc45811e894b93933b2
Vitali Moiseenok <moiseenokv@gmail.com>
2019-10-07 14:45:50 +0300

    feat:add tic-tac-toe class

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,101 @@
 class TicTacToe {
     constructor() {
+        this.player = 'x';
+        this.step = 0;
+        this.field = [[null, null, null],
+                      [null, null, null],
+                      [null, null, null]];
+    }
 
+    changePlayer(){
+        if(this.player=='x'){
+            this.player='o'
+        }else{
+            this.player='x';
+        }
     }
 
     getCurrentPlayerSymbol() {
-
+      return this.player;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if(this.field[rowIndex][columnIndex]===null){
+            if(this.player=='x'){
+                this.field[rowIndex][columnIndex]=1;
+            }else{
+                this.field[rowIndex][columnIndex]=0;
+            }
+            this.step++;
+            this.changePlayer();
+        }
     }
 
     isFinished() {
-
+        const winner= this.getWinner();
+        if(winner !== null || this.step >=9){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     getWinner() {
+        if(
+            (this.field[0][0]==1  && this.field[0][1]==1 && this.field[0][2]==1) ||
+            (this.field[1][0]==1  && this.field[1][1]==1 && this.field[1][2]==1) ||
+            (this.field[2][0]==1  && this.field[2][1]==1 && this.field[2][2]==1) ||
 
+            (this.field[0][0]==1  && this.field[1][0]==1 && this.field[2][0]==1) ||
+            (this.field[0][1]==1  && this.field[1][1]==1 && this.field[2][1]==1) ||
+            (this.field[0][2]==1  && this.field[1][2]==1 && this.field[2][2]==1) ||
+
+            (this.field[0][0]==1  && this.field[1][1]==1 && this.field[2][2]==1) ||
+            (this.field[0][2]==1  && this.field[1][1]==1 && this.field[2][0]==1)
+            ){
+                return 'x';
+            }else if(
+                (this.field[0][0]==0  && this.field[0][1]==0 && this.field[0][2]==0) ||
+                (this.field[1][0]==0  && this.field[1][1]==0 && this.field[1][2]==0) ||
+                (this.field[2][0]==0  && this.field[2][1]==0 && this.field[2][2]==0) ||
+
+                (this.field[0][0]==0  && this.field[1][0]==0 && this.field[2][0]==0) ||
+                (this.field[0][1]==0  && this.field[1][1]==0 && this.field[2][1]==0) ||
+                (this.field[0][2]==0  && this.field[1][2]==0 && this.field[2][2]==0) ||
+
+                (this.field[0][0]==0  && this.field[1][1]==0 && this.field[2][2]==0) ||
+                (this.field[0][2]==0  && this.field[1][1]==0 && this.field[2][0]==0)
+            ){
+                return 'o';
+            }
+        return null;
     }
 
     noMoreTurns() {
-
+        if(this.step < 9){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     isDraw() {
-
+        if(this.isFinished() == false || this.getWinner() != null){
+            return false
+        }else{
+            return true;
+        }
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        if(this.field[rowIndex][colIndex]==1){
+            return 'x'
+        }else if(this.field[rowIndex][colIndex]==0){
+            return 'o'
+        }else{
+            return null;
+        }
+        
     }
 }
 
